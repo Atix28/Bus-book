4dbdecb49153ab645acdff581038df2fcf53ec68
Isuru Liyanage <isuruliyanage1@gmail.com>
2019-09-29 11:31:50 +0530

    Updates

---
+++ precache-manifest.61efa55327fb48b396c5e641149c8a16.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6197d0e2a9d362b4981613c0ea22845",
+    "url": "/Bus-book/index.html"
+  },
+  {
+    "revision": "903d7041fad42580da2d",
+    "url": "/Bus-book/static/css/2.126430be.chunk.css"
+  },
+  {
+    "revision": "d7bdf4231a852c7dc43f",
+    "url": "/Bus-book/static/css/main.c6281679.chunk.css"
+  },
+  {
+    "revision": "903d7041fad42580da2d",
+    "url": "/Bus-book/static/js/2.fba94769.chunk.js"
+  },
+  {
+    "revision": "d7bdf4231a852c7dc43f",
+    "url": "/Bus-book/static/js/main.aafcbc41.chunk.js"
+  },
+  {
+    "revision": "f809d5e5d14c808c3cfc",
+    "url": "/Bus-book/static/js/runtime-main.75a1f762.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Bus-book/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Bus-book/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Bus-book/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Bus-book/static/media/notification.c0d3c94c.eot"
+  }
+]);
